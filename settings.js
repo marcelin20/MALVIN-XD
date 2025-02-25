c663fc353a3837cc716212cbbf2a67731e18e5ea
marcelin20 <linconnudedicace@gmail.com>
2025-02-24 22:52:05 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~JhwVUBqQ#yyF27meIR-rXEfk7Mg7yz4yjiHxcQ6TSp3vuw3yJvDw",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
